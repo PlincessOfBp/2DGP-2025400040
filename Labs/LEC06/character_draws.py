59165690a9b6a151ef26cdebf59ff4d28d895640
PlincessOfBp <72638443+PlincessOfBp@users.noreply.github.com>
2026-09-24 21:01:29 +0900

    사각운동 방향바꾸기

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -57,6 +57,8 @@ def MoveRectangle():
             y += 10
             pass
         elif way == 1:
+            if x <= 250:
+                way += 1
             x -= 10
             pass
         elif way == 2:
