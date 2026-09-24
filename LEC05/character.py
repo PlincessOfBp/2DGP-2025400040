96aaaa1ebf0e203b1d01f1525860da75ff87f45f
PlincessOfBp <72638443+PlincessOfBp@users.noreply.github.com>
2026-09-24 19:18:10 +0900

    개발 시작. While True pass 작성

--- LEC05/character.py
+++ LEC05/character.py
@@ -1,17 +1,43 @@
-from pico2d import *
-
-
-open_canvas(800, 600)
-
 # 여기를 채우시오.
 
+import math
 
+pi = 3.14
+center_x, center_y = 300, 300  # 회전 중심
+radius = 150  # 회전 반지름
+angle = 0  # 초기 각도 (라디안)
+speed = 0.01  # 회전 속도
 
+from pico2d import *
 
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('character.png')
 
+grass.draw(400, 30)
+character.draw(400, 90)
+update_canvas()
 
+x = 50
+y = 50
+situation = 0 
+goto = 0
+while goto<5:
+    clear_canvas()
+    grass.draw(400, 30)
+    character.draw(x, y)
+    update_canvas()
 
-delay(2)
+    angle += speed
+    x = center_x + radius * math.cos(angle)
+    y = center_y + radius * math.sin(angle)
+
+    if angle%360 == 0:
+        goto += 1
+    
+    delay(0.01)
+
+delay(5)
 
 close_canvas()
 
